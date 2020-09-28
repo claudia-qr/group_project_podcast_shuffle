8e8796d645fba959a7f54a11c7b8c14480f6c623
claudia_qr <quezada.claudia.e@gmail.com>
2020-09-27 22:19:57 -0700

    more updates

--- shuffle_app/urls.py
+++ shuffle_app/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('register', views.register),
+    path('login', views.login),
+    path('account/<int:id>', views.account),
+    path('home', views.home),
+    path('library', views.library),
+    path('logout', views.logout),
 ]
